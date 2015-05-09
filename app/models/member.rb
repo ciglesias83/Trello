d090e1f1df38d367c26fe45daa3c79a542f60e72
ciglesias83 <ciglesias@uninorte.edu.co>
2015-05-09 16:46:20 +0000

    Instalacion de Devise

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,8 @@
 class Member < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
     has_many :tasks
     validates :name, presence: true
     validates :lastname, presence: true
